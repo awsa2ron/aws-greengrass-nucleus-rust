94cd74583cae1baf66b0b55849fa398991b1ce03
Aaron Tsui <kaixu@amazon.com>
2022-06-06 15:24:07 +0800

    add tracing lib

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,84 @@
 use clap::Parser;
+use tracing::{info, Level};
+use tracing_subscriber;
 
 pub enum OverallStatus {
     HEALTHY,
-    UNHEALTHY
+    UNHEALTHY,
 }
 
 pub const detailedStatus: &str = "detailedStatus";
 pub const failureCause: &str = "failureCause";
 
-
 struct StatusDetails {
-    pub DETAILED_STATUS_KEY:String,
-    pub FAILURE_CAUSE_KEY:String,
+    pub DETAILED_STATUS_KEY: String,
+    pub FAILURE_CAUSE_KEY: String,
 }
-// implements Chunkable<ComponentStatusDetails> 
-pub struct  FleetStatusDetails{
-    ggcVersion:String,
+// implements Chunkable<ComponentStatusDetails>
+pub struct FleetStatusDetails {
+    ggcVersion: String,
 
-    platform:String,
+    platform: String,
 
-    architecture:String,
+    architecture: String,
 
-    thing:String,
+    thing: String,
 
-    overallStatus:OverallStatus ,
+    overallStatus: OverallStatus,
 
-    sequenceNumber:usize,
+    sequenceNumber: usize,
 
     // List<ComponentStatusDetails> componentStatusDetails,
-
-    // DeploymentInformation deploymentInformation,
-
+    deploymentInformation: DeploymentInformation,
     // pub void setVariablePayload(List<ComponentStatusDetails> variablePayload) {
     //     this.setComponentStatusDetails(variablePayload),
     // }
 }
 
-pub const STATUS_KEY:&str = "status";
-pub const STATUS_DETAILS_KEY:&str = "statusDetails";
-pub const ARN_FOR_STATUS_KEY:&str = "fleetConfigurationArnForStatus";
-pub struct  DeploymentInformation {
-    status:String,
-    statusDetails:StatusDetails,
-    fleetConfigurationArnForStatus:String,
+pub const STATUS_KEY: &str = "status";
+pub const STATUS_DETAILS_KEY: &str = "statusDetails";
+pub const ARN_FOR_STATUS_KEY: &str = "fleetConfigurationArnForStatus";
+pub struct DeploymentInformation {
+    status: String,
+    statusDetails: StatusDetails,
+    fleetConfigurationArnForStatus: String,
 }
 
-pub struct  ComponentStatusDetails {
-    componentName:String,
+pub struct ComponentStatusDetails {
+    componentName: String,
 
-    version:String,
+    version: String,
 
     // List<String> fleetConfigArns,
-
-    statusDetails:String,
+    statusDetails: String,
 
     // We need to add this since during serialization, the 'is' is removed.
-    isRoot:bool,
-
+    isRoot: bool,
     // State state,
 }
 
-pub const FLEET_STATUS_SERVICE_TOPICS:&str = "FleetStatusService";
-pub const DEFAULT_FLEET_STATUS_SERVICE_PUBLISH_TOPIC:&str = "$aws/things/{thingName}/greengrassv2/health/json";
-pub const FLEET_STATUS_TEST_PERIODIC_UPDATE_INTERVAL_SEC :&str= "fssPeriodicUpdateIntervalSec";
-pub const DEFAULT_PERIODIC_PUBLISH_INTERVAL_SEC:usize = 86_400;
-pub const FLEET_STATUS_PERIODIC_PUBLISH_INTERVAL_SEC:&str = "periodicStatusPublishIntervalSeconds";
-const  FLEET_STATUS_SEQUENCE_NUMBER_TOPIC:&str = "sequenceNumber";
-const  FLEET_STATUS_LAST_PERIODIC_UPDATE_TIME_TOPIC:&str = "lastPeriodicUpdateTime";
-const  MAX_PAYLOAD_LENGTH_BYTES:usize = 128_000;
-pub const  DEVICE_OFFLINE_MESSAGE:&str = "Device not configured to talk to AWS IoT cloud. FleetStatusService is offline";
-// extends GreengrassService 
-pub struct  FleetStatusService{
+pub const FLEET_STATUS_SERVICE_TOPICS: &str = "FleetStatusService";
+pub const DEFAULT_FLEET_STATUS_SERVICE_PUBLISH_TOPIC: &str =
+    "$aws/things/{thingName}/greengrassv2/health/json";
+pub const FLEET_STATUS_TEST_PERIODIC_UPDATE_INTERVAL_SEC: &str = "fssPeriodicUpdateIntervalSec";
+pub const DEFAULT_PERIODIC_PUBLISH_INTERVAL_SEC: usize = 86_400;
+pub const FLEET_STATUS_PERIODIC_PUBLISH_INTERVAL_SEC: &str = "periodicStatusPublishIntervalSeconds";
+const FLEET_STATUS_SEQUENCE_NUMBER_TOPIC: &str = "sequenceNumber";
+const FLEET_STATUS_LAST_PERIODIC_UPDATE_TIME_TOPIC: &str = "lastPeriodicUpdateTime";
+const MAX_PAYLOAD_LENGTH_BYTES: usize = 128_000;
+pub const DEVICE_OFFLINE_MESSAGE: &str =
+    "Device not configured to talk to AWS IoT cloud. FleetStatusService is offline";
+// extends GreengrassService
+pub struct FleetStatusService {
     // DeviceConfiguration deviceConfiguration;
     // GlobalStateChangeListener handleServiceStateChange = this::handleServiceStateChange,
     // Function<Map<String, Object>, Boolean> deploymentStatusChanged = this::deploymentStatusChanged,
-
-    updateTopic:String,
-    thingName:String,
+    updateTopic: String,
+    thingName: String,
     // MqttClient mqttClient,
     // Kernel kernel,
-    architecture:String,
-    platform:String,
+    architecture: String,
+    platform: String,
     // MqttChunkedPayloadPublisher<ComponentStatusDetails> publisher,
     // DeploymentStatusKeeper deploymentStatusKeeper,
     //For testing
@@ -89,21 +87,19 @@ pub struct  FleetStatusService{
     // AtomicBoolean isEventTriggeredUpdateInProgress = new AtomicBoolean(false),
     // AtomicBoolean isFSSSetupComplete = new AtomicBoolean(false),
     // Set<GreengrassService> updatedGreengrassServiceSet =
-            // Collections.newSetFromMap(new ConcurrentHashMap<>()),
+    // Collections.newSetFromMap(new ConcurrentHashMap<>()),
     // ConcurrentHashMap<GreengrassService, Instant> serviceFssTracksMap = new ConcurrentHashMap<>(),
     // AtomicBoolean isDeploymentInProgress = new AtomicBoolean(false),
     // Object periodicUpdateInProgressLock = new Object(),
-
-    periodicPublishIntervalSec:usize,
+    periodicPublishIntervalSec: usize,
     // ScheduledFuture<?> periodicUpdateFuture,
-
 }
 
-pub const COMMAND:&str = r#"$ sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassCore/lib/Greengrass.jar 
+pub const COMMAND: &str = r#"$ sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE -jar ./GreengrassCore/lib/Greengrass.jar 
                             --aws-region ap-southeast-1 --thing-name GreengrassQuickStartCore-1 --component-default-user ggc_user:ggc_group 
                             --provision true
                         "#;
-pub const FLOW:&str = r#"Provisioning AWS IoT resources for the device with IoT Thing Name: [GreengrassQuickStartCore-new]...
+pub const FLOW: &str = r#"Provisioning AWS IoT resources for the device with IoT Thing Name: [GreengrassQuickStartCore-new]...
                         Found IoT policy "GreengrassV2IoTThingPolicy", reusing it
                         Creating keys and certificate...
                         Attaching policy to certificate...
@@ -146,6 +142,23 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    // install global collector configured based on RUST_LOG env var.
+    // export RUST_LOG=DEBUG
+    tracing_subscriber::fmt::init();
 
-    println!("{} {} {} {} {}", args.name, args.root, args.log, args.aws_region, args.provision)
+    // this creates a new event, outside of any spans.
+    info!( "{} {} {} {} {}",
+        args.name, args.root, args.log, args.aws_region, args.provision
+    )
+}
+
+fn uploadFleetStatusServiceData(
+    overAllStatus: OverallStatus,
+    deploymentInformation: DeploymentInformation,
+) {
+        // if (!isConnected.get()) {
+        if (true) {
+            info!("Not updating fleet status data since MQTT connection is interrupted.");
+            return;
+        }
 }
